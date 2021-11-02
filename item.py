ee47fa396604c3c90fe2e1f315da39c8aca217cb
Numa Gout <numagout@gmail.com>
2021-11-02 10:26:43 +0100

    New class

---
+++ item.py
@@ -0,0 +1,4 @@
+def moyenne(x, y):
+    return ((x+y)/2)
+
+print(moyenne(5,2))
---
+++ negociation.py
@@ -0,0 +1,9 @@
+class Negociation:
+
+    def __init__(self, service, dateDebut, acheteurs, vendeurs):
+        self.service = service
+        self.dateDebut = dateDebut
+        self.acheteurs = acheteurs
+        self.vendeurs = vendeurs
+
+    
