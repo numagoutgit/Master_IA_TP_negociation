cb580dd3d6bae381808904ae2c751fe8d84face0
Numa Gout <numagout@gmail.com>
2021-11-10 17:07:25 +0100

    Test d'execution

--- test.py
+++ test.py
@@ -5,5 +5,6 @@ from negociation import *
 item = Item(0, 200)
 
 #Test d'execution
-jeu = Negociation(item,5,5,5,2,2,2)
-jeu.run()
+for k in range(100):
+    jeu = Negociation(item,5,5,5,2,2,2)
+    jeu.run()
